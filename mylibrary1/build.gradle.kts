a8fa632241c812f33d1980c0eb005af878af3074
syz <zhengshengyou753@gmail.com>
2024-01-25 11:54:01 -0500

    change

--- mylibrary1/build.gradle.kts
+++ mylibrary1/build.gradle.kts
@@ -8,7 +8,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.tjddn753"
             artifactId = "my-library"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
